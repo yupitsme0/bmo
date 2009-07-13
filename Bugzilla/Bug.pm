9f4aa61cd9aec090a99593dcc68c9e770a03e31a
Reed Loden <reed@mozilla.com>
2009-07-13 12:46:09 -0700

    Fixes for bug 480398, bug 479636, bug 478548, bug 478164, and bug 502769.
    ------------- This line and the following will be ignored --------------
    
    modified:
      Bugzilla/Bug.pm
      template/en/default/attachment/edit.html.tmpl
      template/en/default/bug/create/create.html.tmpl
      template/en/default/bug/edit.html.tmpl
      template/en/default/bug/field.html.tmpl
      template/en/default/bug/show.xml.tmpl

--- Bugzilla/Bug.pm
+++ Bugzilla/Bug.pm
@@ -3325,14 +3325,14 @@ sub check_can_change_field {
         }
     }
 
-    # Only users in the appropriate drivers group can change the cf_(blocking|status)_* fields.
-    if ($field =~ /^cf_(blocking|status)_/) {
+    # Only users in the appropriate drivers group can change the cf_blocking_* fields.
+    if ($field =~ /^cf_blocking_/) {
         unless ($newvalue eq '---' || $newvalue eq '?' || ($oldvalue eq '0' && $newvalue eq '1')) {
             my $drivers_group;
             if ($field eq 'cf_blocking_fennec') {
                 $drivers_group = 'fennec-drivers';
             }
-            if ($field eq 'cf_blocking_191' || $field eq 'cf_status_191') {
+            if ($field eq 'cf_blocking_191') {
                 $drivers_group = 'mozilla-stable-branch-drivers';
             }
             if (!$drivers_group || !$user->in_group($drivers_group)) {
@@ -3341,6 +3341,19 @@ sub check_can_change_field {
             }
         }
     }
+
+    if ($field =~ /^cf_status_/) {
+        # Only drivers can set wanted.
+        if ($newvalue eq 'wanted' && !$user->in_group('mozilla-stable-branch-drivers')) {
+            $$PrivilegesRequired = 3;
+            return 0;
+        }
+        # Require 'canconfirm' to change anything else
+        if (!$user->in_group('canconfirm', $self->{'product_id'})) {
+            $$PrivilegesRequired = 3;
+            return 0;
+        }
+     }
 
     # Allow anyone with (product-specific) "editbugs" privs to change anything.
     if ($user->in_group('editbugs', $self->{'product_id'})) {
