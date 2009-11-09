cc63747d8b3889b23935947f955aa3e7e95ca402
David Miller <justdave@mozilla.com>
2009-11-09 03:38:43 -0500

    Req 57: Get remote IP from X-Forwarded-For if we're behind a trusted proxy.

--- Bugzilla.pm
+++ Bugzilla.pm
@@ -107,6 +107,21 @@ sub init_page {
         };
     }
 
+    ### MOZILLA HACK FOR NETSCALER PASS-THROUGH ###
+    if (i_am_cgi() && ($ENV{REMOTE_ADDR} eq "10.2.82.4")) {
+      my @ips = split(/[, ]+/, $ENV{HTTP_X_FORWARDED_FOR});
+      my @trusted_proxies = ('59.151.50.247','59.151.50.248');
+      while (my $remoteip = pop @ips) {
+        next if grep { $_ eq $remoteip } @trusted_proxies;
+        $ENV{REMOTE_ADDR} = $remoteip;
+        last;
+      }
+    }
+    #if (i_am_cgi() && ($ENV{REMOTE_ADDR} =~ /^166\.87\.255\./)) {
+    #  print Bugzilla->cgi->redirect("/nuisancebugs2.html");
+    #  exit;
+    #}
+
     # If Bugzilla is shut down, do not allow anything to run, just display a
     # message to the user about the downtime and log out.  Scripts listed in 
     # SHUTDOWNHTML_EXEMPT are exempt from this message.
