af5a00d05c54b7a38b46fdb4d645652d331b99ce
David Miller <justdave@mozilla.com>
2010-04-28 14:27:15 -0700

    Make the bzr update fix the file permissions automatically after updating so we don't have to screw with checksetup.pl unless there are dependency or schema changes (the webhead deployment scripts take care of the templates)

--- bzr-update.sh
+++ bzr-update.sh
@@ -3,3 +3,4 @@ echo "+ bzr pull --overwrite -rtag:current-production"
 output=`bzr pull --overwrite -rtag:current-production 2>&1`
 echo "$output"
 echo "$output" | grep "Now on revision" | sed -e 's/Now on revision //' -e 's/\.$//' | xargs -i{} echo bzr pull --overwrite -r{} \# `date` >> `dirname $0`/cvs-update.log
+contrib/fixperls.pl
