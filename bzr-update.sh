69b1c6790e4727a8219d7e992806556e7135658a
David Miller <justdave@mozilla.com>
2010-05-21 03:13:06 -0700

    make bzr-update pull a different tag depending which host you run it on

--- bzr-update.sh
+++ bzr-update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "+ bzr pull --overwrite -rtag:current-production"
-output=`bzr pull --overwrite -rtag:current-production 2>&1`
+HOST=`hostname -s`
+TAG="current-staging"
+[ "$HOST" == "mradm02" ] && TAG="current-production"
+echo "+ bzr pull --overwrite -rtag:$TAG"
+output=`bzr pull --overwrite -rtag:$TAG 2>&1`
 echo "$output"
 echo "$output" | grep "Now on revision" | sed -e 's/Now on revision //' -e 's/\.$//' | xargs -i{} echo bzr pull --overwrite -r{} \# `date` >> `dirname $0`/cvs-update.log
-contrib/fixperls.pl
+contrib/fixperms.pl
