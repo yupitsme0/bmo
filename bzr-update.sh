50d11b97e34bdc4e8abeeb6138b81a124529ec36
David Miller <justdave@mozilla.com>
2011-02-04 14:16:57 -0800

    make it obvious where the error comes from when a new module dependency screws up the permissions fixer

--- bzr-update.sh
+++ bzr-update.sh
@@ -6,4 +6,5 @@ echo "+ bzr pull --overwrite -rtag:$TAG"
 output=`bzr pull --overwrite -rtag:$TAG 2>&1`
 echo "$output"
 echo "$output" | grep "Now on revision" | sed -e 's/Now on revision //' -e 's/\.$//' | xargs -i{} echo bzr pull --overwrite -r{} \# `date` >> `dirname $0`/cvs-update.log
+echo "+ contrib/fixperms.pl"
 contrib/fixperms.pl
