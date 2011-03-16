65b2eb9401857b877fea7b4ed534355cd01b9e55
Byron Jones <bjones@mozilla.com>
2011-03-16 17:14:20 +0800

    rework sanitizeme, for greater sanity and add support for keeping groups,passwords,etc

--- contrib/sanitizeme.pl
+++ contrib/sanitizeme.pl
@@ -20,6 +20,7 @@
 # Contributor(s): Myk Melez <myk@mozilla.org>
 #                 Alex Brugh <alex@cs.umn.edu>
 #                 Dave Miller <justdave@mozilla.com>
+#                 Byron Jones <glob@mozilla.com>
 
 use strict;
 
@@ -29,117 +30,146 @@ use Bugzilla;
 use Bugzilla::Constants;
 use Bugzilla::Util;
 
+use Getopt::Long;
+
 my $dbh = Bugzilla->dbh;
 
 # This SQL is designed to sanitize a copy of a Bugzilla database so that it 
 # doesn't contain any information that can't be viewed from a web browser by
 # a user who is not logged in.                                              
 
-# Last validated against Bugzilla version 3.4.2
+# Last validated against Bugzilla version 4.0
 
-sub delete_product {
-    # This sub really should be Bugzilla::Product->remove_from_db(),
-    # but that didn't exist yet at the time of writing, so I had to
-    # duplicate code from editproducts.cgi instead
-    my $product = shift;
+my ($dry_run, $from_cron, $keep_attachments, $keep_groups,
+    $keep_passwords, $keep_insider, $trace) = (0, 0, 0, '', 0, 0, 0);
+my $keep_groups_sql = '';
 
-    if ($product->bug_count) {
-        foreach my $bug_id (@{$product->bug_ids}) {
-            my $bug = new Bugzilla::Bug($bug_id);
-            $bug->remove_from_db();
+GetOptions(
+    "dry-run" => \$dry_run,
+    "from-cron" => \$from_cron,
+    "keep-attachments" => \$keep_attachments,
+    "keep-passwords" => \$keep_passwords,
+    "keep-insider" => \$keep_insider,
+    "keep-groups:s" => \$keep_groups,
+    "trace" => \$trace,
+) or exit;
+
+if ($keep_groups ne '') {
+    my @groups;
+    foreach my $group_id (split(/\s*,\s*/, $keep_groups)) {
+        my $group;
+        if ($group_id =~ /\D/) {
+            $group = Bugzilla::Group->new({ name => $group_id });
+        } else {
+            $group = Bugzilla::Group->new($group_id);
+        }
+        die "Invalid group '$group_id'\n" unless $group;
+        push @groups, $group->id;
+    }
+    $keep_groups_sql = "NOT IN (" . join(",", @groups) . ")";
+}
+
+$dbh->{TraceLevel} = 1 if $trace;
+
+if ($dry_run) {
+    print "** dry run : no changes to the database will be made **\n";
+    $dbh->bz_start_transaction();
+}
+eval {
+    delete_non_public_products();
+    delete_secure_bugs();
+    delete_insider_comments() unless $keep_insider;
+    delete_security_groups();
+    delete_sensitive_user_data();
+    delete_attachment_data() unless $keep_attachments;
+    print "All done!\n";
+    $dbh->bz_rollback_transaction() if $dry_run;
+};
+if ($@) {
+    $dbh->bz_rollback_transaction() if $dry_run;
+    die "$@" if $@;
+}
+
+sub delete_non_public_products {
+    # Delete all non-public products, and all data associated with them
+    my @products = Bugzilla::Product->get_all();
+    my $mandatory = CONTROLMAPMANDATORY;
+    foreach my $product (@products) {
+        # if there are any mandatory groups on the product, nuke it and
+        # everything associated with it (including the bugs)
+        my $mandatorygroups = $dbh->selectcol_arrayref("SELECT group_id FROM group_control_map WHERE product_id = ? AND (membercontrol = $mandatory)", undef, $product->id);
+        if (0 < scalar(@$mandatorygroups)) {
+            print "Deleting product '" . $product->name . "'...\n";
+            $product->remove_from_db();
         }
     }
-
-    my $comp_ids = $dbh->selectcol_arrayref('SELECT id FROM components
-                                             WHERE product_id = ?',
-                                             undef, $product->id);
-
-    $dbh->do('DELETE FROM component_cc WHERE component_id IN
-              (' . join(',', @$comp_ids) . ')') if scalar(@$comp_ids);
-
-    $dbh->do("DELETE FROM components WHERE product_id = ?",
-             undef, $product->id);
-
-    $dbh->do("DELETE FROM versions WHERE product_id = ?",
-             undef, $product->id);
-
-    $dbh->do("DELETE FROM milestones WHERE product_id = ?",
-             undef, $product->id);
-
-    $dbh->do("DELETE FROM group_control_map WHERE product_id = ?",
-             undef, $product->id);
-
-    $dbh->do("DELETE FROM flaginclusions WHERE product_id = ?",
-             undef, $product->id);
-
-    $dbh->do("DELETE FROM flagexclusions WHERE product_id = ?",
-             undef, $product->id);
-
-    $dbh->do("DELETE FROM products WHERE id = ?",
-             undef, $product->id);
 }
 
-# Delete all non-public products, and all data associated with them
-my @products = Bugzilla::Product->get_all();
-my $mandatory = CONTROLMAPMANDATORY;
-foreach my $product (@products) {
-    # if there are any mandatory groups on the product, nuke it and
-    # everything associated with it (including the bugs)
-    my $mandatorygroups = $dbh->selectcol_arrayref("SELECT group_id FROM group_control_map WHERE product_id = ? AND (membercontrol = $mandatory)", undef, $product->id);
-    if (0 < scalar(@$mandatorygroups)) {
-        print "Deleting product '" . $product->name . "'...\n";
-        delete_product($product);
+sub delete_secure_bugs {
+    # Delete all data for bugs in security groups.
+    my $buglist = $dbh->selectall_arrayref(
+        $keep_groups
+        ? "SELECT DISTINCT bug_id FROM bug_group_map  WHERE group_id $keep_groups_sql"
+        : "SELECT DISTINCT bug_id FROM bug_group_map"
+    );
+    $|=1; # disable buffering so the bug progress counter works
+    my $numbugs = scalar(@$buglist);
+    my $bugnum = 0;
+    print "Deleting $numbugs bugs in " . ($keep_groups ? 'non-' : '') . "security groups...\n";
+    foreach my $row (@$buglist) {
+        my $bug_id = $row->[0];
+        $bugnum++;
+        print "\r$bugnum/$numbugs" unless $from_cron;
+        my $bug = new Bugzilla::Bug($bug_id);
+        $bug->remove_from_db();
+    }
+    print "\rDone            \n" unless $from_cron;
+}
+
+sub delete_insider_comments {
+    # Delete all 'insidergroup' comments and attachments
+    print "Deleting 'insidergroup' comments and attachments...\n";
+    $dbh->do("DELETE FROM longdescs WHERE isprivate = 1");
+    $dbh->do("DELETE attach_data FROM attachments JOIN attach_data ON attachments.attach_id = attach_data.id WHERE attachments.isprivate = 1");
+    $dbh->do("DELETE FROM attachments WHERE isprivate = 1");
+    $dbh->do("UPDATE bugs_fulltext SET comments = comments_noprivate");
+}
+
+sub delete_security_groups {
+    # Delete all security groups.
+    print "Deleting " . ($keep_groups ? 'non-' : '') . "security groups...\n";
+    $dbh->do("DELETE user_group_map FROM groups JOIN user_group_map ON groups.id = user_group_map.group_id WHERE groups.isbuggroup = 1");
+    $dbh->do("DELETE group_group_map FROM groups JOIN group_group_map ON (groups.id = group_group_map.member_id OR groups.id = group_group_map.grantor_id) WHERE groups.isbuggroup = 1");
+    $dbh->do("DELETE group_control_map FROM groups JOIN group_control_map ON groups.id = group_control_map.group_id WHERE groups.isbuggroup = 1");
+    $dbh->do("UPDATE flagtypes LEFT JOIN groups ON flagtypes.grant_group_id = groups.id SET grant_group_id = NULL WHERE groups.isbuggroup = 1");
+    $dbh->do("UPDATE flagtypes LEFT JOIN groups ON flagtypes.request_group_id = groups.id SET request_group_id = NULL WHERE groups.isbuggroup = 1");
+    if ($keep_groups) {
+        $dbh->do("DELETE FROM groups WHERE isbuggroup = 1 AND id $keep_groups_sql");
+    } else {
+        $dbh->do("DELETE FROM groups WHERE isbuggroup = 1");
     }
 }
 
-# Delete all data for bugs in security groups.
-my $buglist = $dbh->selectall_arrayref("SELECT DISTINCT bug_id FROM bug_group_map");
-print "Deleting bugs in security groups...\n";
-$|=1; # disable buffering so the bug progress counter works
-my $numbugs = scalar(@$buglist);
-my $bugnum = 0;
-foreach my $row (@$buglist) {
-    my $bug_id = $row->[0];
-    $bugnum++;
-    print "\r$bugnum/$numbugs";
-    my $bug = new Bugzilla::Bug($bug_id);
-    $bug->remove_from_db();
+sub delete_sensitive_user_data {
+    # Remove sensitive user account data.
+    print "Deleting sensitive user account data...\n";
+    $dbh->do("UPDATE profiles SET cryptpassword = 'deleted'") unless $keep_passwords;
+    $dbh->do("DELETE FROM profiles_activity");
+    $dbh->do("DELETE FROM profiles_search");
+    $dbh->do("DELETE FROM namedqueries");
+    $dbh->do("DELETE FROM tokens");
+    $dbh->do("DELETE FROM logincookies");
+    $dbh->do("DELETE FROM login_failure");
+    $dbh->do("DELETE FROM ts_error");
+    $dbh->do("DELETE FROM ts_exitstatus");
+    $dbh->do("DELETE FROM ts_funcmap");
+    $dbh->do("DELETE FROM ts_job");
+    $dbh->do("DELETE FROM ts_note");
 }
-print "\rDone            \n";
 
-# Delete all 'insidergroup' comments and attachments
-print "Deleting 'insidergroup' comments and attachments...\n";
-$dbh->do("DELETE FROM longdescs WHERE isprivate = 1");
-$dbh->do("DELETE attach_data FROM attachments JOIN attach_data ON attachments.attach_id = attach_data.id WHERE attachments.isprivate = 1");
-$dbh->do("DELETE FROM attachments WHERE isprivate = 1");
-$dbh->do("UPDATE bugs_fulltext SET comments = comments_noprivate");
-
-# Delete all security groups.
-print "Deleting security groups...\n";
-$dbh->do("DELETE FROM bug_group_map");
-$dbh->do("DELETE user_group_map FROM groups JOIN user_group_map ON groups.id = user_group_map.group_id WHERE groups.isbuggroup = 1");
-$dbh->do("DELETE group_group_map FROM groups JOIN group_group_map ON (groups.id = group_group_map.member_id OR groups.id = group_group_map.grantor_id) WHERE groups.isbuggroup = 1");
-$dbh->do("DELETE group_control_map FROM groups JOIN group_control_map ON groups.id = group_control_map.group_id WHERE groups.isbuggroup = 1");
-$dbh->do("DELETE FROM groups where isbuggroup = 1");
-
-# Remove sensitive user account data.
-print "Deleting sensitive user account data...\n";
-$dbh->do("UPDATE profiles SET cryptpassword = 'deleted'");
-$dbh->do("DELETE FROM profiles_activity");
-$dbh->do("DELETE FROM namedqueries");
-$dbh->do("DELETE FROM tokens");
-$dbh->do("DELETE FROM logincookies");
-$dbh->do("DELETE FROM login_activity");
-$dbh->do("DELETE FROM ts_error");
-$dbh->do("DELETE FROM ts_exitstatus");
-$dbh->do("DELETE FROM ts_funcmap");
-$dbh->do("DELETE FROM ts_job");
-$dbh->do("DELETE FROM ts_note");
-
-# Delete unnecessary attachment data.
-print "Removing attachment data to preserve disk space...\n";
-$dbh->do("UPDATE attach_data SET thedata = ''");
-
-print "All done!\n";
-exit;
+sub delete_attachment_data {
+    # Delete unnecessary attachment data.
+    print "Removing attachment data to preserve disk space...\n";
+    $dbh->do("UPDATE attach_data SET thedata = ''");
+}
 
